8e6b92d01b5239ef0c9ea6da7e66ef68701e4dc7
realMNohgee <clund25@gmail.com>
2022-05-31 12:28:23 -0500

    I am starting over :-(

--- mastermind.rb
+++ mastermind.rb
@@ -5,39 +5,8 @@
 # Or let player choose to be the code chooser (person who chooses 4 colors or numbers) #
 # Make sure the program gives the proper feedback (if correct and in the right spot or if just correct) to the codebreaker #
 # Something to figure out if the game has been completed successfully #
-# Give messaged at end for either winning or loosing #
+# Give message at end for either winning or loosing player #
 # After getting game to work, go back and make so computer makes choice on who does what role #
 # Try to implement a strategy for the computer to follow #
-
-class MastermindGame 
-  
-  def initialize
-    @user_play = false
-    @computer_play = false
-    @turn = 1
-    @guesses = 0
-    @good_guesses = []
-    @clue_digit = 0
-    @clue_position = 0
-    @clue_add = true
-    @one_timer = true
-    start_game
-  end
-
-  def start_game
-    puts "You Must crack the code that the AI will decide! Press 1 and enter to start.  Good Luck!"
-    repsone = gets.chomp.to_f
-    if repsone == 1
-      @user_play = true
-      start_users_game
-      elsif repsone == 2
-        @computer_play = true
-        start_users_game
-        else
-          puts "Wrong selection! Please enter 1 to play."
-          start_game
-        end
-      end
-
 
 
