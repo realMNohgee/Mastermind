d45ce546a3f1c72d081ceb2afd36de3f13f89e09
realMNohgee <clund25@gmail.com>
2022-05-31 12:43:20 -0500

    starting with a module that has all my opening and closing statements and the rules to the game.

--- mastermind.rb
+++ mastermind.rb
@@ -9,4 +9,34 @@
 # After getting game to work, go back and make so computer makes choice on who does what role #
 # Try to implement a strategy for the computer to follow #
 
+module OnePlayerMode
+  def create
+    Array.new(4) { rand(1..6) }
+  end
+
+  def starting_message
+    puts 'Welcome to Code Breaker!
+    
+    Please enter a number 4 digits long with the numbers being only 1-6.
+    
+    You will be given clues for digits that are correct and for digits that are correct and in the correct position.
+
+    You will have 12 chances to break the code.
+    
+    Good luck!'
+  end
+
+  def losers_end_message
+    puts 'Sorry you ran out of tries without breaking the code.  Please try again.'
+  end
+
+  def winners_end_message
+    puts 'Way to go! You broke the code!  I see a job at the NSA in you future!'
+  end
+
+  def secret_numbers
+    puts 'The secret code was #{secret}'
+  end
+
+  
 
